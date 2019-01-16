38116a82b2db6def2cdde5ea113e038e47911488
AJEET KUMAR <ajeet.gupta51@gmail.com>
2019-01-16 11:52:31 +0530

    Removed TGMPA

--- functions.php
+++ functions.php
@@ -164,63 +164,6 @@ function levelup_custom_excerpt_length( $length ) {
 }
 add_filter( 'excerpt_length', 'levelup_custom_excerpt_length', 999 );
 
-
-/**
- * Include the TGM_Plugin_Activation class.
- */
-require_once dirname( __FILE__ ) . '/class-tgm-plugin-activation.php';
-
-
-add_action( 'tgmpa_register', 'levelup_register_required_plugins' );
-/**
- * Register the required plugins for this theme.
- * The variable passed to tgmpa_register_plugins() should be an array of plugin
- * arrays.
- *
- * This function is hooked into tgmpa_init, which is fired within the
- * TGM_Plugin_Activation class constructor.
- */
-function levelup_register_required_plugins() {
-	/*
-	 * Array of plugin arrays. Required keys are name and slug.
-	 * If the source is NOT from the .org repo, then source is also required.
-	 */
-	$plugins = array(
-
-	array(
-			'name'      => 'AMP for WP',
-			'slug'      => 'accelerated-mobile-pages',
-			'required'  => false,
-		),
-	);
-
-	/*
-	 * Array of configuration settings. Amend each line as needed.
-	 *
-	 * TGMPA will start providing localized text strings soon. If you already have translations of our standard
-	 * strings available, please help us make TGMPA even better by giving us access to these translations or by
-	 * sending in a pull-request with .po file(s) with the translations.
-	 *
-	 * Only uncomment the strings in the config array if you want to customize the strings.
-	 */
-	$config = array(
-		'id'           => 'tgmpa',                 // Unique ID for hashing notices for multiple instances of TGMPA.
-		'default_path' => '',                      // Default absolute path to bundled plugins.
-		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'parent_slug'  => 'themes.php',            // Parent menu slug.
-		'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-		'has_notices'  => true,                    // Show admin notices or not.
-		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-		'message'      => '',                      // Message to output right before the plugins table.
-
-	);
-
-	tgmpa( $plugins, $config );
-}
-
-
 add_action( 'init', 'levelup_add_editor_styles' );
 /**
  * Apply theme's stylesheet to the visual editor.
